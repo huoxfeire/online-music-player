c72e06b8accdb087fc85a2e44aa2ff79a7a2f927
33 <huoxfei@gmail.com>
2017-04-10 19:02:45 +0800

    have a problem video cross-domain

--- controller/musicList/musicList.js
+++ controller/musicList/musicList.js
@@ -1,5 +1,5 @@
 angular.module("musicApp")
-    .controller("musicListCtrl",["$scope","$http","$rootScope","$routeParams",function($scope,$http,$rootScope,$routeParams){
+    .controller("musicListCtrl",["$scope","$http","$rootScope","$routeParams","$window",function($scope,$http,$rootScope,$routeParams,$window){
         var musicList = $routeParams.musicList;
         var rankID = "";
         angular.forEach($rootScope.rank_type, function(value,key){
@@ -8,10 +8,25 @@ angular.module("musicApp")
             }
         });
         if(rankID){
-            $http.jsonp(musicPlayer.api+"?method=baidu.ting.billboard.billList&type="+rankID+"&size=200&offset=0&callback='success2'")
+            $http.jsonp(musicPlayer.api+"?method=baidu.ting.billboard.billList&type="+rankID+"&size=100&offset=0&callback='success2'")
         }
-        window.success2 = function(data){
+        $window.success2 = function(data){
             console.log(data);
             $scope.song_list = data.song_list;
+        }
+        $scope.play = function($event){
+            if(!$event.target.parentNode.getAttribute("data-no")){
+                console.log("song id 获取失败");
+            }
+            console.log(111);
+            var song_id = $event.target.parentNode.getAttribute("data-no");
+            $http.jsonp(musicPlayer.api+"?method=baidu.ting.song.play&songid="+song_id+"&callback=success3")
+
+        }
+        $window.success3 = function(data){
+            console.log(data);
+            var audio = angular.element("<audio><source src='"+data.bitrate.file_link+"'></audio>");
+            audio.attr('crossOrigin','anonymous');
+            angular.element("body").append(audio);
         }
     }]);
